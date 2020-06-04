2a57faccac5b791bdc190065352fd3e3b3ea29c2
jamesbindin <jamesbindin@gmail.com>
2020-06-04 13:35:21 +0100

    added login and logout nav buttons, as well as create account. login nav button is shown when user is not logged in. The logout nav button is shown as well as the current username is shown when the user is logged in.

--- drawapp/urls.py
+++ drawapp/urls.py
@@ -4,7 +4,7 @@ from drawapp import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('signup/', views.create_user, name='create_user'),
+    path('signup/', views.create_user, name='create_account'),
     path('login/', views.login_user, name='login'),
     path('logout/', views.logout_user, name='logout'),
     ]
