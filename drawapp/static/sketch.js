1eaf2b93c90c8adcd3bf6ebd1b0be42deb42c828
jamesbindin <jamesbindin@gmail.com>
2020-06-16 18:30:46 +0100

    improved the sketchpad page, which allows users to change color and size of the stroke as well as select a colour when the canvas is reset. jquery was used for the events as well as p5.js for the sketching tools functionality its self. styling was also improoved, makeing the page much more user friendly

--- drawapp/static/sketch.js
+++ drawapp/static/sketch.js
@@ -1,29 +1,64 @@
 
 function setup() {
   var c = createCanvas(640, 480);
-  c.parent("jumbotron_id")
-  background(50)
-  noStroke();
-
-  button = createButton('Download');
-  button.position(0, 100);
-  button.mousePressed(download_canvas);
-
-  button2 = createButton('Reset Canvas');
-  button2.position(0, 150);
-  button2.mousePressed(restart_canvas);
+  c.parent("canvas_container")
+  background("#222222")
+  stroke("#FFFFFF")
+  strokeWeight(10)
 }
 
 function draw(){
   if(mouseIsPressed){
-    ellipse(mouseX, mouseY, 30, 30);
+    line(pmouseX, pmouseY, mouseX, mouseY)
   }
 }
 
-function download_canvas(){
-  saveCanvas()
-}
+var background_cp = new iro.ColorPicker('#background_cp', {width:150})
+var stroke_cp = new iro.ColorPicker('#stroke_cp', {width:150})
 
-function restart_canvas(){
-  background(50)
-}
+// expand background colour selector
+$('#btn_background_cp').on('click', ()=>{
+  $('#background_cp').toggle();
+  $('#reset_btn').toggle()
+})
+// change background colour and collapse colour selector
+$('#reset_btn').on('click', ()=>{
+  background(background_cp.color.hexString)
+  $('#background_cp').toggle()
+  $('#reset_btn').toggle()
+})
+
+// expand stroke colour selector
+$('#btn_stroke_cp').on('click', ()=>{
+  $('#stroke_cp').toggle()
+})
+// change stroke color on change
+stroke_cp.on('color:change', function(color) {
+  stroke(color.hexString)
+});
+
+// expand collapse stroke weight
+$('#btn_stroke_weight').on('click', ()=>{
+  $('#stroke_weight_div').toggle()
+})
+// change stroke weight and display value
+$('#stroke_weight_range').on('input',()=>{
+  var val = $('#stroke_weight_range').val()
+  strokeWeight(val)
+  $('#stroke_weight_label').html(val+" px")
+})
+
+// expand collapse download div
+$('#download_btn').on('click', ()=>{
+  $('#download_div').toggle()
+})
+// download sketch
+$('#download_btn_submit').on('click', ()=>{
+  var val = $("#download_input").val()
+  if(val == ''){
+      save()
+  }
+  else {
+    save(val)
+  }
+})
