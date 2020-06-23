9dc681fe47b74f446d2b2b9b95fb163ddc338b39
jamesbindin <jamesbindin@gmail.com>
2020-06-23 13:30:34 +0100

    edited background animation to scale ammount of bubbles on screen to the screensize, also used js6 classes to make code clearer, used an event to clear cursor position when mouse button is released, this also works for touch which fixes a bug on mobile devices

--- drawapp/static/background.js
+++ drawapp/static/background.js
@@ -1,126 +1,102 @@
 function setup(){
   var canvas = createCanvas(windowWidth, windowHeight)
+  var bubbleCount = windowWidth*windowHeight*0.00003
   canvas.parent('p5_div')
-  fill("#50505010")
-  stroke("#50505030")
   strokeWeight(2)
-  angleMode(RADIANS)
 
-  bubbleSwarm = new BubbleSwarm(1000, 20, 50, canvas, 0.05, mouseX, mouseY, 200)
+  bubbleSwarm = new BubbleSwarm(bubbleCount, 20, 30, canvas)
   bubbleSwarm.initilize()
+  bubbleSwarm2 = new BubbleSwarm(bubbleCount, 20, 50, canvas)
+  bubbleSwarm2.initilize()
+  bubbleSwarm3 = new BubbleSwarm(bubbleCount, 30, 80, canvas)
+  bubbleSwarm3.initilize()
 }
 
 function draw(){
   background(255)
-  bubbleSwarm.updateCursor(mouseX, mouseY)
-  bubbleSwarm.checkCursor()
-  bubbleSwarm.show()
-  bubbleSwarm.move()
+  bubbleSwarm.update(mouseX, mouseY, "#50505010", "#ef476f20")
+  bubbleSwarm2.update(mouseX, mouseY, "#50505010", "#ffd16620")
+  bubbleSwarm3.update(mouseX, mouseY, "#50505010", "#06d6a020")
 }
-
 
 function windowResized() {
     resizeCanvas(windowWidth, windowHeight);
-    bubbleSwarm.clear()
     bubbleSwarm.initilize()
+    bubbleSwarm2.initilize()
+    bubbleSwarm3.initilize()
   }
-
-// ----------------------------------------------------------------------------
+  function mouseReleased() {
+    mouseX = null
+    mouseY = null
+  }
+//-----------------------------------------------------------------------------
 class BubbleSwarm{
-  constructor(n, rMin, rMax, canvas, velScaleMax, mouseX, mouseY, max_cur_dist){
+  constructor(n, rMin, rMax, canvas){
     this.bubbles = [];
     this.n = n;
     this.rMin = rMin;
     this.rMax = rMax;
-    this.velScaleMax = velScaleMax;
     this.canvas = canvas;
-    this.pMouseX;
-    this.pMouseY;
-    this.mouseX = mouseX;
-    this.mouseY = mouseY;
-    this.max_cur_dist = max_cur_dist;
-    this.cursorHasMoved = false;
   }
 
   initilize(){
+    this.clear()
     for (var i = 0; i < this.n; i++) {
         var x = random(this.canvas.width)
         var y = random(this.canvas.height)
         var r = random(this.rMin, this.rMax)
-        // var vScale = random(0.05, this.velScaleMax)
-        var vScale = this.velScaleMax
-        var b = new Bubble(x, y, r, vScale)
+        var b = new Bubble(x, y, r)
         this.bubbles.push(b)
       }
   }
 
-  show(){
-    this.bubbles.forEach((b) => {
-      b.show()
-    })
-  }
+  update(mouseX,  mouseY, stroke, fill){
+    this.canvas.stroke(stroke)
+    this.canvas.fill(fill)
 
-  updateCursor(mouseX, mouseY){
-    console.log(this.cursorHasMoved);
-    this.pMouseX = this.mouseX
-    this.pMouseY = this.mouseY
-    this.mouseX = mouseX;
-    this.mouseY = mouseY;
-    if(this.pMouseX == this.mouseX && this.pMouseY == this.mouseY){
-        this.cursorHasMoved = false
-        return;
-    }
-    this.cursorHasMoved = true
-  }
-
-  checkCursor(){
     this.bubbles.forEach((b) => {
-      var  d =  int(dist(b.x, b.y, this.mouseX, this.mouseY))
-      if(d < this.max_cur_dist && this.cursorHasMoved){
-        var dirArc = atan2(b.y-this.mouseY, b.x-this.mouseX) + TWO_PI
-        b.nextPos_x = b.x + (cos(dirArc) * 100)
-        b.nextPos_y = b.y + (sin(dirArc) * 100)
-      }
-      else{
-        b.nextPos_x = b.startX
-        b.nextPos_y = b.startY
-      }
-    })
-  }
-
-  move(){
-    this.bubbles.forEach((b) => {
-      b.move()
+      b.move(mouseX, mouseY)
+      b.show(stroke, fill)
     })
   }
 
   clear(){
     this.bubbles = []
   }
-
-
-
-
 }
-// ----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
 class Bubble{
-  constructor(x, y, r, vScale){
+  constructor(x, y, r){
     this.x = x;
     this.y = y;
+    this.r = r;
     this.startX = x;
     this.startY = y;
-    this.r = r;
-    this.vScale = vScale;
-    this.nextPos_x = x;
-    this.nextPos_y = y;
   }
 
   show(){
     ellipse(this.x, this.y, this.r*2)
   }
 
-  move(){
-    this.x += (this.nextPos_x - this.x) * this.vScale
-    this.y += (this.nextPos_y - this.y) * this.vScale
+  move(mouseX, mouseY){
+    angleMode(RADIANS)
+    var  d =  int(dist(this.x, this.y, mouseX, mouseY))
+    var curDistTollerance = this.r*5
+    var speedScale = 1/this.r
+      if(d < curDistTollerance){
+        var dirArc = atan2(this.y-mouseY, this.x-mouseX) + TWO_PI
+        this.x += (((cos(dirArc)  * curDistTollerance) + this.x) - this.x) * speedScale
+        this.y += (((sin(dirArc)  * curDistTollerance) + this.y) - this.y) * speedScale
+      }
+      else{
+        var px = this.x
+        var py = this.y
+        this.x += (this.startX - this.x) * speedScale
+        this.y += (this.startY - this.y) * speedScale
+        if(int(dist(this.x, this.y, mouseX, mouseY)) < curDistTollerance){
+          this.x = px
+          this.y = py
+        }
+      }
   }
 }
