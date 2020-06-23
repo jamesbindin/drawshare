81d374146579e9a2df9b2821d2044805e17ad2fb
jamesbindin <jamesbindin@gmail.com>
2020-06-23 16:45:09 +0100

    updated sketch page for mobile use. The canvas now fits to the screen when orentation is landscape, the controls are also hidden. the opposite is true in portrait, the canvas is hidden and the controls are shown.

--- drawapp/static/sketch.js
+++ drawapp/static/sketch.js
@@ -1,33 +1,59 @@
 
+var canvas;
 var isMobile = false;
+var orentation = window.screen.orientation.type
+
+// check if device is a mobile device
 if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
   isMobile = true
   }
-$( window ).on( "orientationchange", function( event ) {
-  alert(event.orientation);
-});
-function setup() {
-  var c;
-  if(isMobile){
-    c = createCanvas(windowHeight, windowWidth)
 
-    c.hide()
+// hide and show controls or sketch based on orentation, is triggerd by orientationchange event
+$(window).on( "orientationchange", function( event ) {
+  orentation = window.screen.orientation.type
+    if(orentation.includes("portrait")){
+      canvas.hide()
+      $("#container_id").show()
+    }
+    else if(orentation.includes("landscape")){
+      $("#container_id").hide()
+      canvas.show()
+    }
+});
+
+// p5.js setup function, runs once once elements have loaded. initilises canvas
+// in different ways based on if the device is mobile and the screens orientation.
+function setup() {
+  if(isMobile){
+    var navHeight = $("#navbar").outerHeight()
+    if(orentation.includes("portrait")){
+      canvas = createCanvas(windowHeight, windowWidth - navHeight);
+      canvas.hide()
+    }
+    else if(orentation.includes("landscape")){
+      $("#container_id").hide()
+      canvas = createCanvas(windowWidth, windowHeight - navHeight);
+      canvas.hide()
+      canvas.show()
+    }
   }
   else{
-    c = createCanvas(640, 480);
+    canvas = createCanvas(640, 480);
+    canvas.parent("canvas_container")
   }
-  c.parent("canvas_container")
   background("#222222")
   stroke("#FFFFFF")
   strokeWeight(10)
 }
 
+// draw function for p5.js runs as often as screen refreshes
 function draw(){
     if(mouseIsPressed){
       line(pmouseX, pmouseY, mouseX, mouseY)
     }
   }
 
+// colour pickers from library
 var background_cp = new iro.ColorPicker('#background_cp', {width:150})
 var stroke_cp = new iro.ColorPicker('#stroke_cp', {width:150})
 
